098748cca5bfe54c55451d13bc6330e06b2cc489
OneBillionCat <contactez.bast@gmail.com>
2016-06-06 23:56:56 +0200

    update arduino code

--- Arduino_code/EMG/EMG.ino
+++ Arduino_code/EMG/EMG.ino
@@ -2,10 +2,10 @@
  * RECOPIE DE MOUVEMENT (copy of motion), 2016
  * Update: 06/06/16
  *
- * Installation connecting two arms that one controle the other.
+ * Installation connecting...
  * with EMG Grove > Arduino > Relay > TENS3000
  * 
- * V1
+ * V1.0
  * Written by Bastien DIDIER
  * more info : http://one-billion-cat.com/
  *
@@ -14,33 +14,41 @@
  
 #include <Adafruit_NeoPixel.h>
 
-#define RELAY 4
+#define BUTTON_PIN 3
+#define  RELAY_PIN 4
+#define    LED_PIN 6
 
-#define LED_PIN 6
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(8, LED_PIN, NEO_GRB + NEO_KHZ800);
 
-    uint32_t pixels[8] = {
-      strip.Color(0,255,0),
-      strip.Color(25,225,0),
-      strip.Color(75,200,0),
-      strip.Color(125,125,0),
-      strip.Color(150,125,0),
-      strip.Color(200,75,0),
-      strip.Color(225,25,0),
-      strip.Color(255,0,0)
-    };
+uint32_t led_barGraph[8] = {
+  strip.Color(0,255,0),
+  strip.Color(25,225,0),
+  strip.Color(75,200,0),
+  strip.Color(125,125,0),
+  strip.Color(150,125,0),
+  strip.Color(200,75,0),
+  strip.Color(225,25,0),
+  strip.Color(255,0,0)
+};
 
-//TODO CHANGE SENSIBILITY
-int max_data[4]      = {310,330,350,370};   // max analog data
+//SENSIBILITY
+int SensitivitiesIndex = 2;
+int max_data[7];   // max analog data
+
+//STORE EMG VALUE
+int emg;
+
+int static_analog_dta   = 0;                // static analog data
+//??
 int max_analog_dta      = 350;              // max analog data
 int min_analog_dta      = 100;              // min analog data
-int static_analog_dta   = 0;                // static analog data
 
-// get analog value
+
+// get analog emgue
 int getAnalog(int pin)
 {
     long sum = 0;
-    
+ 
     for(int i=0; i<32; i++)
     {
         sum += analogRead(pin);
@@ -52,73 +60,111 @@ int getAnalog(int pin)
     min_analog_dta = min_analog_dta>dta ? dta : min_analog_dta;         // if min data
     
     return sum>>5;
+    
 }
 
 void setup(){
     Serial.begin(9600);
 
-    pinMode(RELAY, OUTPUT);
-    digitalWrite(RELAY, LOW);
+    pinMode(RELAY_PIN, OUTPUT);
+    digitalWrite(RELAY_PIN, LOW);
+
+    pinMode(BUTTON_PIN, INPUT);
+
+    strip.begin();
+    strip.setBrightness(64);
+    strip.show(); //Initialize led_barGraph to 'off'
     
     long sum = 0;
-
-    for(int i=0; i<=10; i++)
-    {
-        for(int j=0; j<100; j++)
-        {
-            sum += getAnalog(A0);
-            delay(1);
+          
+    for(int i=0; i<=10; i++){
+        for(int j=0; j<100; j++){
+          sum += getAnalog(A0);
+          delay(1);
         }
-        
-        //bar.setLevel(10-i);
+
+        int wait = map(i, 0,10, 0,strip.numPixels());
+        strip.setPixelColor(wait, strip.Color(0,0,255));
+        strip.show();
     }
     
     sum /= 1100;
     
     static_analog_dta = sum;
 
-    Serial.print("static_analog_dta = ");
+    Serial.print("Static Analog Data : ");
     Serial.println(static_analog_dta); 
 
-    strip.begin();
-    strip.show(); // Initialize all pixels to 'off'
+    Serial.println("Setup Max Analog Data:");
+    for(int i=0; i<7; i++){
+      max_data[i] = static_analog_dta + (i+1)*10;
+      Serial.println(max_data[i]);
+    }
+    
+    Serial.println("Ready!");
 }
 
-int level       = 5;
-int level_buf   = 5;
-
-void loop()
-{
-    int val = analogRead(A0);                    // get Analog value
+void loop(){
+  
+    //-----------------------  Switch sensitivity
     
-    int level2;
+    Sensibility();
     
-    if(val>static_analog_dta)                   // larger than static_analog_dta
-    {
-      val = map(val, static_analog_dta, max_data[1], 0, 7); 
-      
-      for(int i=0; i<=val; i++){
-        strip.setPixelColor(i, pixels[i]);
-        strip.show();
-      }
-
-      if(val>=7){
-        digitalWrite(RELAY, HIGH);
-        delay(50);
-      }
+    //-----------------------------  Measure EMG
+    
+    emg = analogRead(A0);
+    
+    //---------------------- Show EMG strength on LED
+    
+    if(emg>static_analog_dta){
+      emg = map(emg, static_analog_dta, max_data[SensitivitiesIndex], 0, 7);
+    } else {
+      emg=-1;
     }
-    /*else 
-    {
-        level2 = 5 - map(val, min_analog_dta, static_analog_dta, 0, 5);
-    }*/
-  
-    
-    digitalWrite(RELAY, LOW);
-      
-    delay(10);
-  
-    for(int i=0; i<=8; i++){
+
+    for(int i=0; i<=emg; i++){
+      strip.setPixelColor(i, led_barGraph[i]);
+      strip.show();
+    }
+    for(int i=emg+1; i<strip.numPixels(); i++){
       strip.setPixelColor(i, strip.Color(0,0,0));
       strip.show();
     }
+
+    //---------------------- Turn ON relay for TENS if emg > THRESHOLD
+    
+    if(emg>=7){
+      digitalWrite(RELAY_PIN, HIGH);
+      delay(50);
+      digitalWrite(RELAY_PIN, LOW);
+    }
+    
+    delay(10);
+}
+
+void Sensibility(){
+  if (digitalRead(BUTTON_PIN)){
+
+    //turn off all the LEDs in LED bar
+    for(int i=0; i<=strip.numPixels(); i++){
+      strip.setPixelColor(i, strip.Color(0,0,0));
+      strip.show();
+    }
+    
+    SensitivitiesIndex++;
+    
+    if( SensitivitiesIndex == strip.numPixels() ){
+      SensitivitiesIndex = 0;
+    }
+    
+    //light up LED at lastSensitivitiesIndex position for visual feedback
+    strip.setPixelColor(SensitivitiesIndex, strip.Color(255,255,255));
+    strip.show();
+        
+    while (digitalRead(BUTTON_PIN)){  
+      delay(10);
+    }       
+    //whait a bit more so that LED light feedback is always visible
+    delay(100);
+  }
 }
